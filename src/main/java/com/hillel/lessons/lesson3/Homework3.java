a8c17c33e10f21e0e669e24d2b06c2c59d4e8c6b
Sergii Kuchinskyi <sergeykuchinsky13@gmail.com>
2022-12-26 18:47:26 +0200

    test

--- src/main/java/com/hillel/lessons/lesson3/Homework3.java
+++ src/main/java/com/hillel/lessons/lesson3/Homework3.java
@@ -2,6 +2,9 @@ package com.hillel.lessons.lesson3;
 
 public class Homework3 {
     public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
 
     }
 }
