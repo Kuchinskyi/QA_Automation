5c3a30f3b1955516b3e2a4f09b7764747b5c0258
Sergii Kuchinskyi <sergeykuchinsky13@gmail.com>
2022-12-26 18:16:04 +0200

    added all files

---
+++ src/main/java/com/hillel/lessons/lesson2/Homework2.java
@@ -0,0 +1,17 @@
+package com.hillel.lessons.lesson2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        System.out.println("Donbass Arena");
+        double N = 48.021186;
+        //System.out.println("North: " + N + '\u00B0');
+        System.out.print("North: ");
+        System.out.print(N);
+        System.out.println('\u00B0');
+        double E = 37.810093;
+        //System.out.println("East: "+ E + '\u00B0');
+        System.out.print("East: ");
+        System.out.print(E);
+        System.out.println('\u00B0');
+    }
+}
