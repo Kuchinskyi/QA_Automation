14b03f71da27e228f08aa31f4d5064522d6cfef5
Sergii Kuchinskyi <sergeykuchinsky13@gmail.com>
2023-01-16 16:24:23 +0200

    Transposition of matrices #2

--- src/main/java/com/hillel/lessons/lesson8/Homework_10.java
+++ src/main/java/com/hillel/lessons/lesson8/Homework_10.java
@@ -7,10 +7,41 @@ public class Homework_10 {
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("Please enter first number");
-        int n = scanner1.nextInt();
+        int n = 0;
+        while (true) {
+            if (scanner1.hasNextInt()) {
+                n = scanner1.nextInt();
+                if (n <= 0) {
+                    System.out.println("Wrong data!");
+                } else {
+                    System.out.println("First number = " + n);
+                    break;
+                }
+
+            } else {
+                System.out.println("Wrong data!");
+                scanner1.nextLine();
+            }
+        }
+
+
         Scanner scanner2 = new Scanner(System.in);
         System.out.println("Please enter second number");
-        int m = scanner2.nextInt();
+        int m = 0;
+        while (true) {
+            if (scanner2.hasNextInt()) {
+                m = scanner2.nextInt();
+                if (m <= 0) {
+                    System.out.println("Wrong data!");
+                } else {
+                    System.out.println("Second number = " + m);
+                    break;
+                }
+            } else {
+                System.out.println("Wrong data!");
+                scanner2.nextLine();
+            }
+        }
         int[][] array = new int[n][m];
         int[][] array2 = new int[m][n];
 
